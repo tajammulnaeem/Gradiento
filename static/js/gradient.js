bbcd45a6a29d97131177cc92756ed6093a1a8740
tajammulnaeem <tajammulnaeemar@gmail.com>
2021-11-08 23:22:57 +0500

    slider for grad-angle added

--- static/js/gradient.js
+++ static/js/gradient.js
@@ -1,13 +1,14 @@
 var copy_text = document.getElementById("grad-text")
-var color1 = document.querySelector(".color1")
-var color2 = document.querySelector(".color2")
-var gradient = document.getElementById("gradient")
-var tooltip = "Copied to clipboard!"
+var color1 = document.getElementById("color1")
+var color2 = document.getElementById("color2")
+var gradient = document.getElementById("gradient-box")
+var angle = document.getElementById("angle") 
 
 
 function setGradient() {
     gradient.style.background =
-        "linear-gradient(45deg, " +
+        "linear-gradient("+ angle.value
+        + "deg, " +
         color1.value +
         " ," +
         color2.value +
@@ -17,9 +18,9 @@ function setGradient() {
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+angle.addEventListener("input",setGradient)
 
 
 function copy_to_clipboard() {
     navigator.clipboard.writeText(copy_text.textContent);
-    
 }
